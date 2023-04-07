ad0bcd74d5e61241bfb43642aa2019f1605b3cd3
Wesley_ <47366440+wesleysousaa@users.noreply.github.com>
2023-04-06 21:23:20 -0300

    test: Creation UserDTOTest

---
+++ src/test/java/br/edu/ifpb/dac/sistemadehorarios/unity/UserDTOTest.java
@@ -0,0 +1,24 @@
+package br.edu.ifpb.dac.sistemadehorarios.unity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import br.edu.ifpb.dac.sistemadehorarios.DTO.UserDTO;
+import br.edu.ifpb.dac.sistemadehorarios.entity.User.UserModel;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDTOTest {
+    @Test
+    public void convertTest(){
+        List<UserModel> listUser = new ArrayList<UserModel>();
+        UserModel user = new UserModel();
+        user.setName("Fulano");
+        user.setEnrollment("12345678");
+        listUser.add(user);
+
+        List<UserDTO> listDTO = new ArrayList<UserDTO>();
+        assertEquals(listDTO , UserDTO.convert(listUser));
+    }
+}
