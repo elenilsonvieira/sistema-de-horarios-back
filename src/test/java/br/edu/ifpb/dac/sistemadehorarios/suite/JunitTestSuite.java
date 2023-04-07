74f93f51e57238ac07a03500bba589b191f1fe77
Wesley_ <47366440+wesleysousaa@users.noreply.github.com>
2023-04-06 21:22:57 -0300

    test: Creation Suite

---
+++ src/test/java/br/edu/ifpb/dac/sistemadehorarios/suite/JunitTestSuite.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.sistemadehorarios.suite;
+
+import br.edu.ifpb.dac.sistemadehorarios.unity.UserDTOTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        UserDTOTest.class,
+})
+
+public class JunitTestSuite {}
