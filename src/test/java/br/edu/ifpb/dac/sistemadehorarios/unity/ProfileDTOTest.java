15869b7a3bd962a92b75c4f71a88cedf215f2eb8
Wesley_ <47366440+wesleysousaa@users.noreply.github.com>
2023-04-08 20:28:15 -0300

    test: Profile DTO

---
+++ src/test/java/br/edu/ifpb/dac/sistemadehorarios/unity/ProfileDTOTest.java
@@ -0,0 +1,33 @@
+package br.edu.ifpb.dac.sistemadehorarios.unity;
+
+import br.edu.ifpb.dac.sistemadehorarios.DTO.ProfileDTO;
+import br.edu.ifpb.dac.sistemadehorarios.DTO.UserDTO;
+import br.edu.ifpb.dac.sistemadehorarios.entity.Profile.ProfileModel;
+import br.edu.ifpb.dac.sistemadehorarios.entity.User.UserModel;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProfileDTOTest {
+
+    @Test
+    @DisplayName("Test Conversion Model to DTO")
+    public void convertTest(){
+        List<ProfileModel> listProfile = new ArrayList<ProfileModel>();
+        ProfileModel profileModel = new ProfileModel();
+        profileModel.setStandard(1);
+        profileModel.setField("12345678");
+        listProfile.add(profileModel);
+
+        List<ProfileDTO> listDTO = new ArrayList<ProfileDTO>();
+        listDTO.add(new ProfileDTO(profileModel));
+
+        assertEquals(listDTO.get(0).getStandard() , ProfileDTO.convert(listProfile).get(0).getStandard());
+        assertEquals(listDTO.get(0).getField() , ProfileDTO.convert(listProfile).get(0).getField());
+
+    }
+}
