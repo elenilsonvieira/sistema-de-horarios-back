3747d0df275f1ba80bf7c7fe2955fb550bcc23d1
wesleysousaa <siginimitsig@gmail.com>
2023-05-18 09:56:09 -0300

    test: refactor SuapServiceIntegration

--- src/test/java/br/edu/ifpb/dac/sistemadehorarios/integration/SuapServiceIntegration.java
+++ src/test/java/br/edu/ifpb/dac/sistemadehorarios/integration/SuapServiceIntegration.java
@@ -1,5 +1,7 @@
 package br.edu.ifpb.dac.sistemadehorarios.integration;
 
+import br.edu.ifpb.dac.sistemadehorarios.entity.Calendar.CalendarModel;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,4 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SuapServiceIntegration {
 
+    @BeforeAll
+    public static void setUp() {
+
+    }
 }
