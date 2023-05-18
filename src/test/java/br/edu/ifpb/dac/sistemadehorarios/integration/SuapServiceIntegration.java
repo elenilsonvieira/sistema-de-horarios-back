df2e2748946ac635f321cde34231ce906aed8eb0
wesleysousaa <siginimitsig@gmail.com>
2023-05-18 09:51:53 -0300

    test: SuapServiceIntegration

---
+++ src/test/java/br/edu/ifpb/dac/sistemadehorarios/integration/SuapServiceIntegration.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.dac.sistemadehorarios.integration;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class SuapServiceIntegration {
+
+}
